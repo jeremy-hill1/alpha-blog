5860c7affaa07d112de8d3cd51316b80e3c60e87
CrazedCoder <okcowboy80@yahoo.com>
2019-06-29 21:06:52 -0500

    Add edit update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+
+  #post 'users', to: 'users#create'
   # Another way to add signup pat
-  # resources :users, except: [:new]
+  resources :users, except: [:new]
   # We make exception for ":new" because
     # we have a 'new' path already set up for articles
-    
+
 end
