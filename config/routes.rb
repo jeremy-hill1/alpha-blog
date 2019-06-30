4be85729a1779257ffb7f704908d563ce99a0033
CrazedCoder <okcowboy80@yahoo.com>
2019-06-29 20:18:25 -0500

    signup users from ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'home' => 'pages#home'
   root 'pages#home'
   get 'about' => 'pages#about'
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  # Another way to add signup pat
+  # resources :users, except: [:new]
+  # We make exception for ":new" because
+    # we have a 'new' path already set up for articles
+    
 end
