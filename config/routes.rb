9bc928584002f6151c4951001980751c8baed40f
CrazedCoder <okcowboy80@yahoo.com>
2019-07-01 22:56:30 -0500

    Add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   #post 'users', to: 'users#create'
   # Another way to add signup pat
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # We make exception for ":new" because
     # we have a 'new' path already set up for articles
 
