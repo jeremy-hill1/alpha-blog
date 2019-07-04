554537044ce7ee1df92ef4ff6b8d0f390113a899
CrazedCoder <okcowboy80@yahoo.com>
2019-07-04 17:12:35 -0500

    Complete authentication system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
 def index
-
+  rendirect_to articles_path if logged_in?
 end
 
 def about
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,7 @@ def new
 end
 
 def create
-
-  user = user.find_by(email: params[:session][:email].downcase)
+  user = User.find_by(email: params[:session][:email].downcase)
   if user && user.authenticate(params[:session][:password])
     session[:user_id] = user.id
     flash[:success] = "You have successfully logged in"
