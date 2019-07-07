aea31aeace96098adf2286b7e2e17d8ad369df07
CrazedCoder <okcowboy80@yahoo.com>
2019-07-07 18:25:13 -0500

    Add category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,6 +18,22 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @cagegory = Category.find(params[:id])
+  end
+
+  def update
+    @cagegory = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Name was successfully updated"
+      redirect_to category_path(@category)  
+    else
+      render 'edit'
+    end
+  end
+
+  end
+
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
